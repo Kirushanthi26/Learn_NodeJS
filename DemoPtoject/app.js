1cad32b50ae6429aabee88dd764e13da12c56860
kirushanthi26 <kirushanthikiru26@gmail.com>
2022-11-29 12:52:24 +0530

    404 page completd

--- DemoPtoject/app.js
+++ DemoPtoject/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 const express = require('express');
 const app = express();
@@ -7,5 +8,9 @@ const adminRoute = require('./routes/admin.js');
 
 app.use(adminRoute);
 app.use(shopRoute);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 
 app.listen(3060);
